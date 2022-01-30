59aae22eb1490f5fa2b63f44f1aea90ce9cb45ed
Stévillis Sousa <stevillis@hotmail.com>
2022-01-30 15:35:35 -0400

    Create model serializers

---
+++ cursos/serializers.py
@@ -0,0 +1,30 @@
+from rest_framework import serializers
+
+from cursos.models import Avaliacao, Curso
+
+
+class AvaliacaoSerializer(serializers.ModelSerializer):
+    class Meta:
+        extra_kwargs = {"email": {"write_only": True}}
+        model = Avaliacao
+        fields = (
+            "id",
+            "curso",
+            "nome",
+            "email",
+            "comentario",
+            "avaliacao",
+            "criacao",
+            "ativo",
+        )
+
+
+class CursoSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Curso
+        fields = (
+            "id",
+            "titulo",
+            "criacao",
+            "ativo",
+        )
