ab737717a77213136a005a62895463556093bc2c
Stévillis Sousa <stevillis@hotmail.com>
2022-03-07 15:48:47 -0400

    List Avaliacao and Avaliacoes by Curso

--- cursos/views.py
+++ cursos/views.py
@@ -1,5 +1,6 @@
 from rest_framework.generics import (ListCreateAPIView,
-                                     RetrieveUpdateDestroyAPIView)
+                                     RetrieveUpdateDestroyAPIView,
+                                     get_object_or_404)
 
 from cursos.models import Avaliacao, Curso
 from cursos.serializers import AvaliacaoSerializer, CursoSerializer
@@ -19,7 +20,20 @@ class AvaliacoesAPIView(ListCreateAPIView):
     queryset = Avaliacao.objects.all()
     serializer_class = AvaliacaoSerializer
 
+    def get_queryset(self):
+        curso_pk = self.kwargs.get('curso_pk')
+        if curso_pk:
+            return self.queryset.filter(curso_id=curso_pk)
+        return self.queryset.all()
+
 
 class AvaliacaoAPIView(RetrieveUpdateDestroyAPIView):
     queryset = Avaliacao.objects.all()
     serializer_class = AvaliacaoSerializer
+
+    def get_object(self):
+        curso_pk = self.kwargs.get('curso_pk')
+        avaliacao_pk = self.kwargs.get('avaliacao_pk')
+        if curso_pk:
+            return get_object_or_404(self.get_queryset(), curso_id=curso_pk, pk=avaliacao_pk)
+        return get_object_or_404(self.get_queryset(), pk=avaliacao_pk)
