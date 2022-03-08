8bd9107a47075f1839b43f68c7c60c3bffc352d7
Stévillis Sousa <stevillis@hotmail.com>
2022-03-08 15:36:22 -0400

    HyperLinked Related Field

--- cursos/serializers.py
+++ cursos/serializers.py
@@ -14,7 +14,10 @@ class AvaliacaoSerializer(serializers.ModelSerializer):
 
 class CursoSerializer(serializers.ModelSerializer):
     # Nested Relationship
-    avaliacoes = AvaliacaoSerializer(many=True, read_only=True)
+    # avaliacoes = AvaliacaoSerializer(many=True, read_only=True)
+
+    # HyperLinked Related Field
+    avaliacoes = serializers.HyperlinkedRelatedField(many=True, read_only=True, view_name='avaliacao-detail')
 
     class Meta:
         model = Curso
