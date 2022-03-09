3c31718b7f5580b7d2912386650921fc3a6330d6
Stévillis Sousa <stevillis@hotmail.com>
2022-03-08 23:29:52 -0400

    Test DELETE Method with assert

---
+++ teste_delete.py
@@ -0,0 +1,16 @@
+import requests
+
+headers = {
+    'Authorization': 'Token 22c4f0770323557ee4d67258c66e9df5d7660fde'
+}
+BASE_URL = 'http://localhost:8000/api/v2/'
+
+atualizacao = {
+    'titulo': 'Libras para Refugiados',
+    'url': 'https://www.google.com/courses/libras-para-refugiados'
+}
+
+response = requests.delete(url=f'{BASE_URL}cursos/4/', headers=headers)
+
+assert response.status_code == 204
+assert len(response.text) == 0
